29a444b3271861107ebc439805c1219e4fa61d2b
Richard Chin <richardchinloy@hotmail.com>
2017-10-22 17:01:17 +0100

    resizeable, accepts user threshold values.

--- MandelbrotBrowser/Form1.Designer.cs
+++ MandelbrotBrowser/Form1.Designer.cs
@@ -31,42 +31,76 @@
             this.PictureBox = new System.Windows.Forms.PictureBox();
             this.labelHeader = new System.Windows.Forms.Label();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.buttonRender = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxThreshold = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxPalette = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.statusStrip.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // PictureBox
             // 
+            this.PictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.PictureBox.BackColor = System.Drawing.Color.White;
-            this.PictureBox.Location = new System.Drawing.Point(133, 56);
+            this.PictureBox.Location = new System.Drawing.Point(133, 63);
             this.PictureBox.Name = "PictureBox";
-            this.PictureBox.Size = new System.Drawing.Size(640, 480);
+            this.PictureBox.Size = new System.Drawing.Size(640, 508);
             this.PictureBox.TabIndex = 0;
             this.PictureBox.TabStop = false;
+            this.PictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox_MouseMove);
             // 
             // labelHeader
             // 
-            this.labelHeader.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelHeader.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.labelHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.labelHeader.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelHeader.ForeColor = System.Drawing.Color.White;
-            this.labelHeader.Location = new System.Drawing.Point(133, 8);
+            this.labelHeader.Location = new System.Drawing.Point(12, 24);
             this.labelHeader.Name = "labelHeader";
-            this.labelHeader.Size = new System.Drawing.Size(640, 36);
+            this.labelHeader.Size = new System.Drawing.Size(761, 36);
             this.labelHeader.TabIndex = 1;
-            this.labelHeader.Text = "HEADER";
+            this.labelHeader.Text = "Mandelbot";
+            this.labelHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // statusStrip
             // 
-            this.statusStrip.Location = new System.Drawing.Point(0, 573);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar,
+            this.statusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 608);
             this.statusStrip.Name = "statusStrip";
             this.statusStrip.Size = new System.Drawing.Size(785, 22);
             this.statusStrip.TabIndex = 2;
             this.statusStrip.Text = "statusStrip1";
             // 
+            // toolStripProgressBar
+            // 
+            this.toolStripProgressBar.Name = "toolStripProgressBar";
+            this.toolStripProgressBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(668, 17);
+            this.statusLabel.Spring = true;
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
             // buttonRender
             // 
-            this.buttonRender.Location = new System.Drawing.Point(645, 546);
+            this.buttonRender.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRender.Location = new System.Drawing.Point(646, 578);
             this.buttonRender.Name = "buttonRender";
             this.buttonRender.Size = new System.Drawing.Size(127, 27);
             this.buttonRender.TabIndex = 3;
@@ -74,18 +108,99 @@
             this.buttonRender.UseVisualStyleBackColor = true;
             this.buttonRender.Click += new System.EventHandler(this.buttonRender_Click);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(785, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonReset.Location = new System.Drawing.Point(513, 577);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(127, 27);
+            this.buttonReset.TabIndex = 5;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Threshold";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBoxThreshold
+            // 
+            this.textBoxThreshold.Location = new System.Drawing.Point(31, 91);
+            this.textBoxThreshold.Name = "textBoxThreshold";
+            this.textBoxThreshold.Size = new System.Drawing.Size(96, 20);
+            this.textBoxThreshold.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Colour Palette";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // comboBoxPalette
+            // 
+            this.comboBoxPalette.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPalette.FormattingEnabled = true;
+            this.comboBoxPalette.Location = new System.Drawing.Point(31, 145);
+            this.comboBoxPalette.Name = "comboBoxPalette";
+            this.comboBoxPalette.Size = new System.Drawing.Size(96, 21);
+            this.comboBoxPalette.TabIndex = 9;
+            this.comboBoxPalette.SelectedIndexChanged += new System.EventHandler(this.comboBoxPalette_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(785, 595);
+            this.ClientSize = new System.Drawing.Size(785, 630);
+            this.Controls.Add(this.comboBoxPalette);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxThreshold);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonReset);
             this.Controls.Add(this.buttonRender);
             this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.labelHeader);
             this.Controls.Add(this.PictureBox);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Mandelbrot Browser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,6 +212,15 @@
         private System.Windows.Forms.Label labelHeader;
         private System.Windows.Forms.StatusStrip statusStrip;
         private System.Windows.Forms.Button buttonRender;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxThreshold;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxPalette;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
     }
 }
 
--- MandelbrotBrowser/Form1.cs
+++ MandelbrotBrowser/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,15 +18,38 @@ namespace MandelbrotBrowser
         private double _maxX;
         private double _minY;
         private double _maxY;
+        private int _Threshold;         // threshold iteration 
+        private int _colorpalette;      // colour palette selection
+
+        private int _Height;            // height of view window
+        private int _Width;             // width of view window
+        private bool _isRendering;      // indicates current rendering
+        private bool _isResizing;
 
         public Form1()
         {
             InitializeComponent();
 
-            _minX = -2.0;
-            _maxX = 2.0;
-            _minY = -2.0;
-            _maxY = 2.0;
+            _isRendering = false;
+            _isResizing = false;
+            toolStripProgressBar.Visible = false;
+
+            // colour palettes
+            int pos = comboBoxPalette.Items.Add("Greyscale");
+            SetDefaultValues();
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //Render();
         }
 
         //
@@ -34,55 +58,169 @@ namespace MandelbrotBrowser
             Render();
         }
 
-
-        // Main rendering code
-        private void Render()
+        private void buttonReset_Click(object sender, EventArgs e)
         {
-            int Width = PictureBox.Width;
-            int Height = PictureBox.Height;
-
-            Bitmap bitmap = new Bitmap(Width, Height);
-            Graphics graphics = PictureBox.CreateGraphics();
-
-            for(int y = 0; y < Height; y++)
-            {
-                for(int x =0; x < Width; x++)
-                {
-                    Color clr = CalculateEscapeColour(Width, Height, y, x);
-                    bitmap.SetPixel(x, y, clr);
-                }
-                graphics.DrawImage(bitmap, 0, 0);
-            }
-            graphics.DrawImage(bitmap, 0, 0);
+            SetDefaultValues();
+            Render();
         }
 
-
-        /***************************************************************************
-         * 
-         ****************************************************************************/
-        private Color CalculateEscapeColour(int Width, int Height, int y, int x)
+        private void SetDefaultValues()
         {
-            // transform current coordinates to the complex number equivalent
-            double re = ((x * (_maxX - _minX)) / (double)Width) + _minX;
-            double img = ((y * (_maxY - _minY)) / (double)Height) + _minY;
+            _minX = -2.4;
+            _maxX = 1.0;
+            _minY = -1.8;
+            _maxY = 1.8;
+            _colorpalette = 1;
+            _Threshold = 20;
 
+            _Width = PictureBox.Width;
+            _Height = PictureBox.Height;
+
+            comboBoxPalette.SelectedIndex = 0;
+            textBoxThreshold.Text = _Threshold.ToString();
+        }
+
+        private void comboBoxPalette_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            _isResizing = true;
+        }
+
+        /// <summary>
+        /// Render the image if the user has resized the window
+        /// </summary>
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            if (_isResizing)
+            {
+                _isResizing = false;
+                _Width = PictureBox.Width;
+                _Height = PictureBox.Height;
+
+                Render();
+            }
+        }
+
+        /// <summary>
+        /// Main Rendering Code
+        /// </summary>
+        /// <remarks>
+        /// 
+        /// </remarks>
+        private void Render()
+        {
+            // Get the current threshold value in the edit box.
+            // We need to validate this and alert the user if wrong.
+            if( !Int32.TryParse( textBoxThreshold.Text, out _Threshold ) )
+            {
+                MessageBox.Show("Please enter a valid numeric threshold", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            _isRendering = true;
+
+            Bitmap bitmap = new Bitmap(_Width, _Height);
+            Graphics graphics = PictureBox.CreateGraphics();
+
+            // start progress
+            toolStripProgressBar.Value = 0;
+            toolStripProgressBar.Visible = true;
+
+            graphics.FillRectangle(new SolidBrush(Color.White), 0, 0, Width, Height);
+
+            for(int y = 0; y < _Height; y++)
+            {
+                for(int x =0; x < _Width; x++)
+                {
+                    Color clr = CalculateEscapeColour(x, y);
+                    bitmap.SetPixel(x, y, clr);
+                }
+
+                graphics.DrawImage(bitmap, 0, 0);
+
+                // calculate progress
+                int perc = ((_Width * y) * 100) / (_Height * _Width);
+                toolStripProgressBar.Value = perc;
+            }
+
+            toolStripProgressBar.Value = 100;
+            toolStripProgressBar.Visible = false;
+            PictureBox.Image = bitmap;
+
+            _isRendering = false;
+        }
+
+        /// <summary>
+        /// Main calculation
+        /// </summary>
+        /// <param name="Width"></param>
+        /// <param name="Height"></param>
+        /// <param name="y"></param>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        private Color CalculateEscapeColour(int x, int y)
+        {
             aComplexNumber Z = new aComplexNumber(0, 0);
-            aComplexNumber C = new aComplexNumber(re, img);
+            aComplexNumber C = new aComplexNumber();
+
+            TransformCoord(x, y, ref C);
 
             int i = 0;
-            for (i = 0; i < 20; i++)
+            for (i = 0; i < _Threshold; i++)
             {
                 aComplexNumber Zn = Z.Squared() + C;
                 if (Zn.Magnitude() >= 2)
-                    return Color.Black;
+                {
+                    // calculate escape colour based on the number of iterations
+                    int intensity = (i * 255) / _Threshold;
+                    Color color = Color.FromArgb(intensity, intensity, intensity);
+                    return color;
+                }
 
                 Z = Zn;
             }
 
-            // calculate escape colour based on the number of iterations
-            int intensity = (i * 255) / 20;
-            Color color = Color.FromArgb(intensity, intensity, intensity);
-            return color;
+            return Color.Black;
         }
+
+
+
+        /// <summary>
+        /// Displays the coordinates on the screen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isRendering)
+                return; 
+
+            aComplexNumber C = new aComplexNumber();
+            TransformCoord(e.X, e.Y, ref C);
+
+            // output the coordinates to the screen
+            statusLabel.Text = $"x: {C.Real:0.###} y:{C.Imaginary:0.###}";
+        }
+
+        /// <summary>
+        /// Transform the given screen coordinate to the represented complex number.
+        /// </summary>
+        private void TransformCoord(Point screenPoint, ref aComplexNumber complexPoint)
+        {
+            TransformCoord(screenPoint.X, screenPoint.Y, ref complexPoint);
+        }
+
+        /// <summary>
+        /// </summary>
+        private void TransformCoord(int x, int y, ref aComplexNumber complexPoint)
+        {
+            complexPoint.Real = ((x * (_maxX - _minX)) / _Width) + _minX;
+            complexPoint.Imaginary = (((_Height - y) * (_maxY - _minY)) / _Height) + _minY;
+        }
+
     }
 }
